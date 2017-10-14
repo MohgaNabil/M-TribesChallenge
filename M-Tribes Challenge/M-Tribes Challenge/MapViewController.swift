3d757acbe7fedcd840ee292875b1bd4aa95698d4
Mohga Nabil <Mohga@eg.ibm.com>
2017-10-14 20:48:23 +0200

    selecting and deselecting mapping pin

--- M-Tribes Challenge/M-Tribes Challenge/MapViewController.swift
+++ M-Tribes Challenge/M-Tribes Challenge/MapViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 import MapKit
 import SwiftyJSON
 
+enum CarMode{
+	case SINGLE, MULTIPLE
+}
+
 class MapViewController: UIViewController,MKMapViewDelegate {
 
 	@IBOutlet weak var locationsMap: MKMapView!
 	private var locationsInfo : [JSON]?
+	var carMode : CarMode = .MULTIPLE
 	
     override func viewDidLoad() {
         super.viewDidLoad()
+		self.locationsMap.delegate = self
 		self.locationsMap.showsUserLocation = false
 		self.renderMapAnnotations()
     }
@@ -44,4 +50,30 @@ class MapViewController: UIViewController,MKMapViewDelegate {
 		}
 	}
 	
+	func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+		if carMode == .SINGLE{
+			self.renderMapAnnotations()
+			view.setSelected(false, animated: true)
+			carMode = .MULTIPLE
+		}else{
+			let tap = UITapGestureRecognizer(target: self, action: #selector(MapViewController.tapAnnotationPin(gesture:)))
+			view.addGestureRecognizer(tap)
+			self.locationsMap.annotations.forEach({ (annotation) in
+				guard view.annotation != nil && annotation.title! == view.annotation!.title! else{
+					self.locationsMap.removeAnnotation(annotation)
+					return
+				}
+			})
+			carMode = .SINGLE
+		}
+	}
+	
+	
+	func tapAnnotationPin(gesture: UITapGestureRecognizer){
+		if gesture.view is MKAnnotationView{
+			let annotationView = gesture.view as! MKAnnotationView
+			annotationView.removeGestureRecognizer(gesture)
+			self.locationsMap.deselectAnnotation(annotationView.annotation!, animated: true)
+		}
+	}
 }
