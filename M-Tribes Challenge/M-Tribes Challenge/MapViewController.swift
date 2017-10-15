23cbd60c861cb09799882f15d58add94bd2fe52f
Mohga Nabil <Mohga@eg.ibm.com>
2017-10-15 21:31:51 +0200

    update user location piece

--- M-Tribes Challenge/M-Tribes Challenge/MapViewController.swift
+++ M-Tribes Challenge/M-Tribes Challenge/MapViewController.swift
@@ -26,13 +26,17 @@ class MapViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDel
 		//Configure Map
 		self.locationsMap.delegate = self
 		self.locationsMap.setUserTrackingMode(.follow, animated: true)
+		
 		//Configure location
 		self.locationManager = CLLocationManager()
 		self.locationManager.delegate = self
 		self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-		self.renderMapAnnotations()
-//		self.authorizeLocation()
     }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		self.authorizeLocation()
+	}
 
 	/// cascade changes in model data
 	///
@@ -47,15 +51,13 @@ class MapViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDel
 	/// add annotation to map
 	func renderMapAnnotations(){
 		if self.locationsMap != nil && self.locationsInfo != nil{
-			if !self.locationsMap.annotations.isEmpty{
-				self.locationsMap.removeAnnotations(self.locationsMap.annotations)
-			}
-			for userLocation in self.locationsInfo!{
+			self.clearMap()
+			for carLocation in self.locationsInfo!{
 				let locationAnnotation = MKPointAnnotation()
-				let coordinates = userLocation["coordinates"].arrayValue
+				let coordinates = carLocation["coordinates"].arrayValue
 				locationAnnotation.coordinate = CLLocationCoordinate2DMake(coordinates[1].doubleValue, coordinates[0].doubleValue);
-				locationAnnotation.title = userLocation["name"].stringValue
-				locationAnnotation.subtitle = userLocation["address"].stringValue
+				locationAnnotation.title = carLocation["name"].stringValue
+				locationAnnotation.subtitle = carLocation["address"].stringValue
 				self.locationsMap.addAnnotation(locationAnnotation)
 			}
 		}
@@ -95,46 +97,57 @@ class MapViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDel
 		}
 	}
 	
-//	func authorizeLocation(){
-//		let status = CLLocationManager.authorizationStatus()
-//		if status == .notDetermined || status == .denied || status == .authorizedWhenInUse {
-//			locationManager.requestAlwaysAuthorization()
-//			locationManager.requestWhenInUseAuthorization()
-//		}
-//		locationManager.startUpdatingLocation()
-//		locationManager.startUpdatingHeading()
-//	}
+	/// Authorize app to access current location
+	func authorizeLocation(){
+		let status = CLLocationManager.authorizationStatus()
+		if status == .notDetermined || status == .denied || status == .authorizedWhenInUse {
+			locationManager.requestAlwaysAuthorization()
+			locationManager.requestWhenInUseAuthorization()
+		}
+		locationManager.startUpdatingLocation()
+		locationManager.startUpdatingHeading()
+	}
 	
 	/// fetches the user's current location
 	///
 	/// - Parameters:
 	///   - mapView
 	///   - userLocation
-	func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-		let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
-		let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-		
-		self.locationsMap.setRegion(region, animated: true)
-		
-		let currentLocationAnnotation = MKPointAnnotation()
-		currentLocationAnnotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude);
-		currentLocationAnnotation.title = "Current location"
-		currentLocationAnnotation.subtitle = userLocation.subtitle
-		self.locationsMap.addAnnotation(currentLocationAnnotation)
-
-	}
-//	
-//	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//		let userLocation:CLLocation = locations[0] as CLLocation
-//
+//	func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+//		let userLocationJson = ["name":"Current location", "Address":userLocation.subtitle as Any,"coordinates":[userLocation.coordinate.longitude,userLocation.coordinate.latitude] ]
+//		self.locationsInfo?.append(JSON(userLocationJson))
+//		
 //		let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
 //		let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
 //		
 //		self.locationsMap.setRegion(region, animated: true)
 //		
-//		let myAnnotation = MKPointAnnotation()
-//		myAnnotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude);
-//		myAnnotation.title = "Current location"
-//		self.locationsMap.addAnnotation(myAnnotation)
+//		let currentLocationAnnotation = MKPointAnnotation()
+//		currentLocationAnnotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude);
+//		currentLocationAnnotation.title = "Current location"
+//		currentLocationAnnotation.subtitle = userLocation.subtitle
+//		self.locationsMap.addAnnotation(currentLocationAnnotation)
+//
 //	}
+	
+	/// remove all map annotation
+	func clearMap(){
+		if !self.locationsMap.annotations.isEmpty{
+			self.locationsMap.removeAnnotations(self.locationsMap.annotations)
+		}
+	}
+	
+	/// fetches the user's current location
+	///
+	/// - Parameters:
+	///   - manager
+	///   - locations
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		self.locationManager.stopUpdatingLocation()
+		self.locationManager.stopUpdatingHeading()
+		let userLocation = locations[0] as CLLocation
+		let userLocationJson = ["name":"Current location", "Address": "", "coordinates":[userLocation.coordinate.longitude,userLocation.coordinate.latitude]] as [String : Any]
+		self.locationsInfo?.append(JSON(userLocationJson))
+		self.renderMapAnnotations()
+	}
 }
